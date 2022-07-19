160109c9c782224301507327d62a1c556ea4a60a
Chris Giroux <chris.giroux86@gmail.com>
2022-07-19 13:12:26 -0400

    TESTING THIS OUT

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-console.log("TESTING!!");
+function testBot(username, displayName) {
+   return /(\[bot\]|-bot)$/i.test(username) || /\(bot\)$/i.test(displayName || '')
+}
+
+console.log('ONE', testBot('[bot]', 'bot'));
+console.log('TWO', testBot( '-bot', 'bot'));
+console.log('THREE', testBot('(bot)', '(bot)'));
