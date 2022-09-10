4ad790f7f32451e931a2a160b4293927a7e6dea0
justfold101786 <109819360+justfold101786@users.noreply.github.com>
2022-09-10 11:12:52 -0400

    Update test.js
    Co-Authored-By: Chris Giroux <59579733+cgiroux86@users.noreply.github.com>
    Co-Authored-By: girouxchris1017 <79605020+girouxchris1017@users.noreply.github.com>

--- test.js
+++ test.js
@@ -2,4 +2,4 @@ function testBot(username, displayName) {
    return /(\[bot\]|-bot)$/i.test(username) || /\(bot\)$/i.test(displayName || 'HI!')
 }
 
-console.log('HELLO!!!!');
+console.log('HELLO!');
